ddb20921987d8059b95cd7f3a29254fef1573464
CodeCraft9835 <apurvadutta9835@gmail.com>
2026-09-22 16:43:13 +0530

    Google clone

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,5 +1,4 @@
-// Challenge 2:
-// Replace the arguments below according to your preference.
+
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
 favouriteMovieGenre("space")
@@ -15,9 +14,7 @@ favouriteEdgeStyle("round")
 
 
 
-////////////////////////////////////
-// IGONE THE CODE BELOW THIS LINE //
-////////////////////////////////////
+
 
 function setProp(prop, value) {
     document.documentElement.style.setProperty(prop, value)
